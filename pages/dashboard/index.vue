ec34f7b3358fefaedda0ed712440040f471e2f9e
Vicente Manuel Guerra Hernández <vicentemanuelguerrahernandez@MacBook-Air-de-Vicente.local>
2025-06-22 14:21:27 -0500

    Logo in Login, drop-down menu to log-out and now load organization info on lkogin

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,6 +1,17 @@
-<template>Communication that moves at the speed of your product</template>
+
+<template>
+  <div v-if="organization && organization.name" class="text-xl font-semibold text-green-900 mb-2">
+    {{ organization.name }}
+  </div>
+  Communication that moves at the speed of your product
+</template>
 
 <script setup>
+import { storeToRefs } from 'pinia'
+import { useUserStore } from '~/stores/user'
+const userStore = useUserStore()
+const { organization } = storeToRefs(userStore)
+
 useSeoMeta({
 	title: 'RoadFlow AI'
 })
